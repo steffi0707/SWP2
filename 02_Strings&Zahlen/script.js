cc1975320e8546786493c3d258fe9222e38e4169
Steffi <stefanie.trautz@gmail.com>
2023-03-08 08:38:20 +0100

    strings coding

---
+++ 02_Strings&Zahlen/script.js
@@ -0,0 +1,10 @@
+let a = 17.4;
+let b = 3.9;
+
+let result = a / b;
+console.log("The result of " + a  + " devided by " + b + " is " + result);
+
+let vorname = "Steffi";
+let nachname = "Trautz";
+
+console.log("Ich heiße " + vorname + " " + nachname);
