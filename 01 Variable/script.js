227d117265c838945db02e848d03b9e42195fbd5
Steffi <stefanie.trautz@gmail.com>
2023-03-08 08:20:59 +0100

    finish excersise

--- 01 Variable/script.js
+++ 01 Variable/script.js
@@ -9,3 +9,27 @@ let c = 10;
 let result = a + b + c;
 
 console.log("A hat den Wert " + a);
+
+console.log("Das Ergebnis ist " + result);
+
+result = (a + b) / c;
+console.log("Das Ergebnis ist " + result);
+
+result = c - a + b;
+console.log("Das Ergebnis ist " + result);
+
+result = a + b + c * 3;
+console.log("Das Ergebnis ist " + result);
+
+result = (a + b + c) / 4;
+console.log("Das Ergebnis ist " + result);
+
+a = 3;
+b = a + 3;
+c = c - 3;
+
+console.log("A hat den Wert " + a);
+
+console.log("B hat den Wert " + b);
+
+console.log("C hat den Wert " + c);
