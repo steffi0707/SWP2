4518b08f937b421a2f79e69f2daf8a4433564922
Steffi <stefanie.trautz@gmail.com>
2023-03-22 09:05:34 +0100

    finished 05_if-Bedingungen

---
+++ 05_if-Bedingung/script.js
@@ -0,0 +1,23 @@
+let number = 17;
+
+if (number<20){
+    console.log("Mini");
+}
+else if (number>50){
+    console.log("Large");
+}
+else {
+    console.log("Medium");
+}
+
+number =69;
+
+if (number<=7){
+    console.log("Mini");
+}
+else if (number>90){
+    console.log("Large");
+}
+else {
+    console.log("Medium");
+}
