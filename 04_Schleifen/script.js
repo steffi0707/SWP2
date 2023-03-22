9dfd428343f077897431e7934bdd2fb5667ea54c
Steffi <stefanie.trautz@gmail.com>
2023-03-22 08:45:25 +0100

    finished 04_Schleifen

---
+++ 04_Schleifen/script.js
@@ -0,0 +1,49 @@
+console.log("test")
+
+let a = 7;
+let b = 4.3;
+let name = "Steffi"; //Text
+
+console.log(name + " " + b + a);
+
+//Schleife
+
+for (let count = 7; count < 46; count++) {
+    console.log(count);
+}
+
+console.log("Fertig");
+
+for (let count = 9; count < 100; count++) {
+    console.log(count);
+}
+
+console.log("Fertig");
+
+for (let count = 10; count > -1; count--) {
+    console.log(count);
+}
+
+console.log("Fertig");
+
+for (let count = 1234; count > 1206; count--) {
+    console.log(count);
+}
+
+console.log("Fertig");
+
+for (let count = 0; count < 8; count++) {
+    console.log(count + " Steffi");
+}
+
+console.log("Fertig");
+
+for (let count = 0; count < 11; count++) {
+    console.log(count);
+}
+
+console.log(name);
+
+for (let count = 10; count > -1; count--) {
+    console.log(count);
+}
