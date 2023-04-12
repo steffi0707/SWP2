b41371cc4e316f4b2bab7edd655ac033aaaa2b53
Steffi <stefanie.trautz@gmail.com>
2023-04-12 09:11:29 +0200

    Finished 08 Switch Statement

---
+++ 08_Switch_Statement/script.js
@@ -0,0 +1,22 @@
+let randomNumber = Math.random() * 5 + 5; 
+let random = Math.floor(randomNumber);
+
+switch (random) {
+    case 10: 
+        console.log("Ten");
+        break;
+    case 9:
+        console.log("Nine");
+        break;
+    case 8:
+        console.log("Eight");
+        break;
+    case 7:
+        console.log("Seven");
+        break;
+    case 6:
+        console.log("Six");
+        break;
+    case 5: 
+        console.log("Five");
+};
