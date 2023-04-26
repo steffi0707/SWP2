8c7cffe6c4f029b3c8f3cbae953c9f68e98d7cab
Steffi <stefanie.trautz@gmail.com>
2023-04-26 09:15:16 +0200

    finished aufgabe_10

--- 10_Funktionen/scripts.js
+++ 10_Funktionen/scripts.js
@@ -1,17 +1,32 @@
+let a = 12;
+let b = 8;
+let resultadd = a + b;
+let resultsub = a - b;
+let resultmul = a * b;
+let resultcal = (a + b)/2 * a;
+
+console.log(resultadd);
+console.log(resultsub);
+console.log(resultmul);
+console.log(resultcal);
+
+
+//Easterdate
+
 function printEasterDate(year) {
     n = year - 1900
     a = n % 19
-    b  ((7* a + 1) / 19)
+    b = ((7* a + 1) / 19)
     m = (11 * a + 4 - b) % 29
     q = Math.floor (n / 4)
     w = (n + q + 31 - m) % 7
     p = Math.floor (25 - m - w)
 
     if (p > 0) {
-        console.log("Ostern ist der")
+        console.log("Ostern ist der " + p + ". April.")
     }
     else {
-        console.log("OStern ist der");
+        console.log("Ostern ist der" + p + 31 + ". März");
     }
 }
 
