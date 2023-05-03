ee7ce45ffe2670f2e5369dfc9000e56b77344bbc
Steffi <stefanie.trautz@gmail.com>
2023-05-03 09:15:50 +0200

    started 09

--- 09_Aufgabe_Zeichen/script.js
+++ 09_Aufgabe_Zeichen/script.js
@@ -1,1 +1,17 @@
+let a = 1;
+let b = 2;
+
+
+
 //Aufgabe 1
+for (let index = 1; index < 3; index ++) {
+    console.log("XXXX");
+    if (index%1==0) {
+        console.log("OOOO");
+    }
+}
+
+//Aufgabe 2
+for (1-100) {
+    
+}
