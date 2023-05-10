50c3de6d972f561f7595b59d7fde42662ec03a4d
Steffi <stefanie.trautz@gmail.com>
2023-05-10 08:57:02 +0200

    finished 09

--- 09_Aufgabe_Zeichen/script.js
+++ 09_Aufgabe_Zeichen/script.js
@@ -12,6 +12,25 @@ for (let index = 1; index < 3; index ++) {
 }
 
 //Aufgabe 2
-for (1-100) {
-    
+let summe = 0;
+for (let number = 1; number <= 100; number ++) {
+    if (number%2==0) {
+        summe += number;
+    }
+}
+
+console.log("Die Summe ist " + summe);
+
+//Aufgabe 3
+for (let name = 1; name <=20; name ++) {
+    if (name%20==0) {
+        console.log(" XXXX");
+        console.log("X    X");
+        console.log("X    ");
+        console.log("  XX");
+        console.log("     X");
+        console.log("X    X");
+        console.log(" XXXX");
+    }
+
 }
