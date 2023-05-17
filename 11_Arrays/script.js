b1570c00cb1b1fc7479d64fae71ca6f5e987dce2
Steffi <stefanie.trautz@gmail.com>
2023-05-17 08:40:49 +0200

    worked on 11_Arrays

---
+++ 11_Arrays/script.js
@@ -0,0 +1,33 @@
+//Aufgabe 1
+let arr =[4,1,2,3];
+
+console.log(arr);
+
+arr.push(17);
+arr.push(199);
+
+for (let index = 0; index < arr.length; index++) {
+    console.log(arr[index]); 
+    
+}
+
+let summe = 0;
+for (let index = 0; index < arr.length; index++) {
+    summe += arr[index];
+}
+console.log(summe);
+
+let mittelwert = 0;
+for (let index = 0; index < arr.length; index++) {
+    mittelwert += arr[index] / 6;
+}
+
+console.log(mittelwert);
+
+//Aufgabe 2
+let arrays = ["Susi", "Paula", "Hans"];
+
+for (let index = 0; index < array.length; index++) {
+    if ()
+    
+}
