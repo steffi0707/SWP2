1b97c0efbb1bf06fcb6526a8afd2c3370860e603
Steffi <stefanie.trautz@gmail.com>
2023-06-14 08:08:02 +0200

    AUfgabe 12 nochmal bearbeitet

--- 12_Input_from_Shell/03_Read/script.js
+++ 12_Input_from_Shell/03_Read/script.js
@@ -1,28 +1,19 @@
-{
-    "name": "03_read",
-    "version": "1.0.0",
-    "lockfileVersion": 2,
-    "requires": true,
-    "packages": {
-      "": {
-        "name": "03_read",
-        "version": "1.0.0",
-        "license": "ISC",
-        "dependencies": {
-          "readline": "^1.3.0"
-        }
-      },
-      "node_modules/readline": {
-        "version": "1.3.0",
-        "resolved": "https://registry.npmjs.org/readline/-/readline-1.3.0.tgz",
-        "integrity": "sha512-k2d6ACCkiNYz222Fs/iNze30rRJ1iIicW7JuX/7/cozvih6YCkFZH+J6mAFDVgv0dRBaAyr4jDqC95R2y4IADg=="
-      }
-    },
-    "dependencies": {
-      "readline": {
-        "version": "1.3.0",
-        "resolved": "https://registry.npmjs.org/readline/-/readline-1.3.0.tgz",
-        "integrity": "sha512-k2d6ACCkiNYz222Fs/iNze30rRJ1iIicW7JuX/7/cozvih6YCkFZH+J6mAFDVgv0dRBaAyr4jDqC95R2y4IADg=="
-      }
-    }
-  }
+import { createInterface } from "readline";
+
+const readline = createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const readLineAsync = () => {
+  return new Promise((resolve) => {   
+    readline.question("", (userRes) => {
+      resolve(userRes);
+      //readline.close();
+    });
+  });
+};
+
+console.log("Enter your name");
+let inputOfUser = await readLineAsync();
+console.log("Your name is " + inputOfUser);
